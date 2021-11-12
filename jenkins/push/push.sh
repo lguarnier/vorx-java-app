c5db65b7ded878e9d412a7ce84f46998c7c1175f
EC2 Default User <ec2-user@ip-10-0-101-126.ec2.internal>
2021-11-12 02:04:14 +0000

    First commit

---
+++ jenkins/push/push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "********************"
+echo "** Pushing image ***"
+echo "********************"
+
+IMAGE="maven-project"
+
+echo "** Logging in ***"
+docker login -u lguarnier -p $PASS
+
+echo "*** Tagging image ***"
+docker tag $IMAGE:$BUILD_TAG lguarnier/$IMAGE:$BUILD_TAG
+
+echo "*** Pushing image ***"
+docker push lguarnier/$IMAGE:$BUILD_TAG
